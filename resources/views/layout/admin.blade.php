4d409626ca4016c735c7285592e4ed259bd22f9d
perezcontrerasluis <luisperezcontreras@gmail.com>
2017-04-02 01:23:44 -0600

    integracion de bootstrap y laravel collective

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -1,6 +1,17 @@
 <html>
     <head>
         <title>admin secction</title>
+        <!-- Latest compiled and minified CSS 
+        <link rel="stylesheet" href="css/bootstrap.min.css" >
+        -->
+        {!!Html::style('css/bootstrap.min.css')!!}
+
+        <!-- Optional theme 
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">-->
+        {!!Html::style('css/bootstrap-theme.min.css')!!}
+        <!-- Latest compiled and minified JavaScript
+        <script src="js/bootstrap.min.js" ></script> -->
+        {!!Html::script('js/bootstrap.min.js')!!}
     </head>
 
     <body>
