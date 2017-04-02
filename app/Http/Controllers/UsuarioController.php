4538fff6ed5d39711228503810b649244bb4e894
perezcontrerasluis <luisperezcontreras@gmail.com>
2017-04-02 03:11:34 -0500

    formulario con ruta para crear usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -8,11 +8,16 @@ class UsuarioController extends Controller
 {   
     //
     public function index(){
-    	
+
     }
 
     //funcion para crear un usuario
     public function create(){
     	return view('usuario.create');
+    }
+
+    //funcion para guardar usuario
+    public function store(){
+    	return "quieres guardar un usuario";
     }
 }
--- resources/views/usuario/create.blade.php
+++ resources/views/usuario/create.blade.php
@@ -1,19 +1,20 @@
 @extends('layout.admin')
 
 @section('content')
-   <form action="">
+   <!-- <form action="usuario.store" method="post"> -->
+   {!!Form::open(['route' => 'usuario.store','method'=>'POST'])!!}
    	<div class="form-group">
    		<label for="">Nombre</label>
    		<input type="text" class="form-control">
    	</div>
    	<div class="form-group">
    		<label for="">correo</label>
-   		<input type="text" class="form-control">
+   		<input type="mail" class="form-control">
    	</div>
    	<div class="form-group">
    		<label for="">contraseña</label>
-   		<input type="text" class="form-control">
+   		<input type="password" class="form-control">
    	</div>
    	<button class="btn btn-primary">Registrar</button>
-   </form>
+   {!! Form::close() !!}
 @endsection
