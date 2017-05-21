31c677216bacd9c92bfb13426d632cdd56aaf0b3
perezcontrerasluis <luisperezcontreras@gmail.com>
2017-05-21 18:56:00 -0500

    Mostrar nombre de usurio

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -33,7 +33,7 @@
                           <ul class="nav navbar-nav">
                             
                             <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">user <span class="caret"></span></a>
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{!!Auth::user()->name!!} <span class="caret"></span></a>
                               <ul class="dropdown-menu">
                                 <li><a href="#">Login</a></li>
                                 <li role="separator" class="divider"></li>
