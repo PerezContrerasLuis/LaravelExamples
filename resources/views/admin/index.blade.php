937f1badf601c520df6bbd67c5a8a505788b8903
perezcontrerasluis <luisperezcontreras@gmail.com>
2017-05-22 15:07:06 -0500

    welcome admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layout.admin')
+@section('content')
+<div class="flex-center position-ref full-height">
+            
+            <div class="content">
+                <div class="title m-b-md">
+                   <div class="alert alert-success" role="alert">
+                     <h1>Welcome Mr admin...</h1>
+                  </div> 
+                </div>
 
-
+            </div>
+        </div>
+@endsection
 
 
 
