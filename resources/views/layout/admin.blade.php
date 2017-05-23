fb79358fbc02679aff8699f9a0005daa284d1997
perezcontrerasluis <luisperezcontreras@gmail.com>
2017-05-23 13:02:08 -0500

    Menu Acordeon

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -35,7 +35,7 @@
                             <li class="dropdown">
                               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{!!Auth::user()->name!!} <span class="caret"></span></a>
                               <ul class="dropdown-menu">
-                                <li><a href="#">Login</a></li>
+                                <li><a href="#">Ajustes</a></li>
                                 <li role="separator" class="divider"></li>
                                 <li><a href="/logout">Logout</a></li>
                               </ul>
@@ -49,7 +49,7 @@
             </div>
             <div class="row">
                 <div class="col col-md-3">
-                        <div class="navbar-default sidebar" role="navifation">
+                        <!-- <div class="navbar-default sidebar" role="navifation">
                             <div class="sidebar-nav navbar-collapse">
                                 <div class="nav" id="side-menu">
                                     @if(Auth::user()->id == 1)
@@ -89,7 +89,76 @@
                                     </li>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
+                        <!-- Menu acordion  -->
+                         <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                                @if(Auth::user()->id == 1)
+                              <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingOne">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="text-decoration:none; ">
+                                      <h4 class="panel-title">
+                                          Usuarios
+                                      </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                  <div class="panel-body">
+                                    <ul class="nav nav-second-level">
+                                        <li>
+                                            <a href="{!!URL::to('/usuario/create')!!}"><i class='fa fa-plus fa-fw'></i>Agregar</a>
+                                        </li>
+                                        <li>
+                                            <a href="{!!URL::to('/usuario')!!}"><i class='fa fa-list-ol fa-fw'></i>Usuarios</a>
+                                        </li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>
+                               @endif
+                              <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingTwo">
+                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style="text-decoration:none; ">
+                                    <h4 class="panel-title">
+                                      Pelicula
+                                    </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                  <div class="panel-body">
+                                    <ul class="nav nav-second-level">
+                                        <li>
+                                            <a href="#"><i class='fa fa-plus fa-fw'></i>Agregar</a>
+                                        </li>
+                                        <li>
+                                            <a href="#"><i class='fa fa-list-ol fa-fw'></i>Peliculas</a>
+                                        </li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingThree">
+                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style="text-decoration:none; ">
+                                    <h4 class="panel-title">
+                                      Genero
+                                    </h4>
+                                    </a>
+                                </div>
+                                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                                  <div class="panel-body">
+                                    <ul class="nav nav-second-level">
+                                        <li>
+                                            <a href="{!!URL::to('/usuario/create')!!}"><i class='fa fa-plus fa-fw'></i>Agregar</a>
+                                        </li>
+                                        <li>
+                                            <a href="{!!URL::to('/usuario')!!}"><i class='fa fa-list-ol fa-fw'></i>Usuarios</a>
+                                        </li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                        <!-- Fin Menu acordion -->
 
                     <!-- </nav> -->
                 </div>
