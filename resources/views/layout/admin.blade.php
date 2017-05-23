2ca82e58459c6d0827fecb8b4f2cb3841eb391e7
perezcontrerasluis <luisperezcontreras@gmail.com>
2017-05-23 12:11:52 -0500

    Menu de opciones con validacion. modificamos el menu para que se muestre agregar si el usuario es admin

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -33,7 +33,7 @@
                           <ul class="nav navbar-nav">
                             
                             <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">user <span class="caret"></span></a>
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{!!Auth::user()->name!!} <span class="caret"></span></a>
                               <ul class="dropdown-menu">
                                 <li><a href="#">Login</a></li>
                                 <li role="separator" class="divider"></li>
@@ -52,6 +52,7 @@
                         <div class="navbar-default sidebar" role="navifation">
                             <div class="sidebar-nav navbar-collapse">
                                 <div class="nav" id="side-menu">
+                                    @if(Auth::user()->id == 1)
                                     <li>
                                         <a href="#"><i class="fa fa-users fa-fw"></i>Usuario<span class="fa arrow"></span></a>
                                         <ul class="nav nav-second-level">
@@ -63,6 +64,7 @@
                                             </li>
                                         </ul>
                                     </li>
+                                    @endif
                                     <li>
                                         <a href="#"><i class="fa fa-users fa-fw"></i>Pelicula<span class="fa arrow"></span></a>
                                         <ul class="nav nav-second-level">
